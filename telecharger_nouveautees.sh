e159c583351f68f67e119b25c6d2f25c1090fb7d
Normand Cyr <normand.cyr@gmail.com>
2018-10-28 14:41:03 -0400

    ne pas télécharger nouveautés Signature

--- telecharger_nouveautees.sh
+++ telecharger_nouveautees.sh
@@ -4,6 +4,6 @@ URL_cellier="https://www.saq.com/content/SAQ/fr/produits/nouveautes/nouvel-arriv
 URL_signature="https://www.saq.com/content/SAQ/fr/produits/nouveautes/nouvel-arrivage-saq-signature.html"
 
 wget $URL_cellier
-sleep 5
 
-wget $URL_signature
+#sleep 5
+#wget $URL_signature
