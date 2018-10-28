7b0e0e97c8d88be90b73c892534ff392bb94263d
Normand Cyr <normand.cyr@gmail.com>
2018-10-28 14:46:51 -0400

    ajout de commentaires

--- scraper.py
+++ scraper.py
@@ -2,24 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import requests
 import json
 import csv
-from json2table import convert
 
 def extraire_donnees(fichier_nouv_cellier, parser):
 
-    '''
-    requete = requests.get(fichier_nouv_cellier)
-
-    if requete.status_code == requests.codes.ok:
-        # creer la soupe
-        soupe = BeautifulSoup(requete.text, parser)
-    else:
-        # pleurer parce que la page n'est pas disponible
-        requete.raise_for_status()
-    '''
-
+    # création de la soupe à partir du fichier de résultats téléchargé
     with open(fichier_nouv_cellier, 'r') as requete:
         soupe = BeautifulSoup(requete, parser)
 
@@ -37,6 +25,7 @@ def trouver_resultats(soupe):
 
 def extraire_info(resultat_info):
 
+    # extraction des résultats en utilisant BeautifulSoup
     titre = resultat_info.find('div', attrs={'class': 'mev-product-title '}).find('a')['title'].replace('\xa0',' ')
     identite = resultat_info.find('span', attrs={'class': 'dprod-identite'}).text.strip()
     pays = resultat_info.find('span', attrs={'class': 'dprod-pays'}).text.strip()
@@ -44,6 +33,7 @@ def extraire_info(resultat_info):
     code_saq = resultat_info.find('div', attrs={'class': 'dprod-codesaq'}).text.replace('Code SAQ :\xa0', '')
     url = resultat_info.find('div', attrs={'class': 'mev-product-title '}).find('a')['href']
 
+    # certains champs sont optionels alors éviter les erreurs si c'est vide
     appelation = resultat_info.find('span', attrs={'class': 'dprod-appellation'})
     if appelation != None:
         appelation = appelation.text.strip()
@@ -58,6 +48,7 @@ def extraire_info(resultat_info):
 
     #prix = resultat_prix.find('td', attrs={'class': 'price'}).text.strip()
 
+    # construction du dictionaire
     info_produit = {titre: {'appelation': appelation,
                             'cépage': cepage,
                             'identité': identite,
@@ -68,6 +59,7 @@ def extraire_info(resultat_info):
                             'code SAQ': code_saq,
                             'URL': url}}
 
+    # construction de la ligne CSV
     info_produit_csv = [titre, appelation, cepage, identite, pays, region, volume, code_saq, url]
 
     return(info_produit, info_produit_csv)
@@ -81,25 +73,20 @@ def main():
     soupe = extraire_donnees(fichier_nouv_cellier, parser)
     soupe_resultats_info, soupe_resultats_prix = trouver_resultats(soupe)
 
+    # créer la ligne de titre dans le fichier CSV
     with open('sortie.csv', 'w') as fichier_csv:
         fichier_csv.write('titre,appelation,cépage,identité,pays,région,volume,code SAQ,url\n')
 
+    # créer les fichiers JSON et CSV
     for resultat_info in soupe_resultats_info:
         info_produit, info_produit_csv = extraire_info(resultat_info)
-        #print(info_produit_csv)
+
         with open('sortie.json', 'a') as fichier_json:
             fichier_json.write(json.dumps(info_produit, indent = 4)+ '\n')
 
         with open('sortie.csv', 'a') as fichier_csv:
             csvwriter = csv.writer(fichier_csv)
             csvwriter.writerow(info_produit_csv)
-
-    # with open('sortie.json', 'r') as fichier_json:
-    #     objet_json = json.load(fichier_json)
-    #
-    # table_attributes = {"style" : "width:100%"}
-    # html = convert(objet_json, table_attributes=table_attributes)
-    # print(html)
 
 
 if __name__ == '__main__':
